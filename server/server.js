4d19384c91a7351afc8940a423985a4908416fde
Paz Jaldety <paz@coperato.local>
2018-07-03 09:26:36 +0300

    Add new message and create message events.

--- server/server.js
+++ server/server.js
@@ -16,11 +16,22 @@ app.use(express.static(publicPath));
 
 io.on('connection' , (socket) => {
   console.log('New user connected.');
+  socket.emit('newMessage' , {
+    from:'Paz',
+    text:'Hey whats up?',
+    createdAt:123
+  });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('Create Message: ' , newMessage);
+  });
 
   socket.on('disconnect', () => {
     console.log('User disconnected.');
   });
 });
+
+
 
 
 server.listen(port, () => {
